7a5e6f8155f9ca726e9b50fbcc7ca7b78f420dd2
P5V1V3K <113207270+P5V1V3K@users.noreply.github.com>
2024-11-09 16:12:29 +0530

    [845] [Time Beats: 70.66%] [Memory Beats: 89.66%] - LeetPush

---
+++ 845-Longest-Mountain-in-Array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        int c=0;
+        for(int i=1;i<arr.length-1;++i){
+            if(arr[i]>arr[i+1]){
+                int l=i, r=i, rc=0, lc=0;
+                while(l-1>=0){
+                    if(arr[l]>arr[l-1]){
+                        ++lc;
+                        --l;
+                    }
+                    else break;
+                }
+                if(lc!=0)while(r+1<arr.length){
+                    if(arr[r]>arr[r+1]){
+                        ++rc;
+                        ++r;
+                    }
+                    else break;
+                }
+                if(lc!=0 && rc!=0)c=Math.max(1+lc+rc,c);
+            }
+        }
+        return c;
+    }
+}
