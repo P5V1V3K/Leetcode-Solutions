3d7eb5e46baf6ebdadf0d65c87f4dfdf42ef0340
P5V1V3K <113207270+P5V1V3K@users.noreply.github.com>
2024-11-10 10:53:47 +0530

    [125] [Time Beats: 99.09%] [Memory Beats: 59.02%] - LeetPush

--- 125-Valid-Palindrome.java
+++ 125-Valid-Palindrome.java
@@ -2,11 +2,8 @@ class Solution {
     public boolean isPalindrome(String s) {
         if(s.equals(\\))return true;
         s=s.toLowerCase();
-        s=s.replaceAll(\ \,\\);
-        if(s.equals(\\))return true;
         int l=0;
         int r=s.length()-1;
-        System.out.println(s);
         while(l<r){
             char a = s.charAt(l);
             char b = s.charAt(r);
