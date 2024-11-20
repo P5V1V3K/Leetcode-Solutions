01b5b5c03eb3cc62cda63f1b635b620a5c9c2922
Vivek Pratap Singh <113207270+P5V1V3K@users.noreply.github.com>
2024-11-21 00:31:42 +0530

    [231] [Time Beats: 100.00%] [Memory Beats: 67.59%] - LeetPush

--- 231-Power-of-Two.java
+++ 231-Power-of-Two.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<1)return false;
-        if((n&(n-1))==0)return true;
-        return false;
+        return pow(n);
+    }
+    public boolean pow(int n){
+        if(n==1)return true;
+        if(n%2!=0)return false;
+        return pow(n/2);
     }
 }
