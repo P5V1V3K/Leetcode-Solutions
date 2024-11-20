938d6455cc71e09053a6ec7775390f8c877093fe
Vivek Pratap Singh <113207270+P5V1V3K@users.noreply.github.com>
2024-11-21 00:13:03 +0530

    [11] [Time Beats: 90.61%] [Memory Beats: 70.89%] - LeetPush

--- 11-Container-With-Most-Water.java
+++ 11-Container-With-Most-Water.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxArea(int[] height) {
         int ans=0,i=0,j=height.length-1;
         while(i<j){
-            int area = (height[i]<height[j]?height[i]:height[j])*Math.abs((i-j));
+            int area = (height[i]<height[j]?height[i]:height[j])*(j-i);
             ans=(ans>area?ans:area);
             if(height[i]>height[j])--j;
             else ++i;
